aecf97fa7cfb063c9ce0f7fd25a23d4eda8b2f43
jesserangel <jrangel.dev@gmail.com>
2015-05-11 23:01:13 -0430

    Actualizando archivos base

--- index.php
+++ index.php
@@ -6,8 +6,25 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title></title>
 	<link rel="stylesheet" href="css/base.css">
+	<link href='http://fonts.googleapis.com/css?family=Architects+Daughter' rel='stylesheet' type='text/css'>
 </head>
 <body>
-	
+	<header>
+		<h1>Jessé Rangel - Portfolio</h1>
+		<nav>
+			<ul id="menu">
+				<li><a href="#">Inicio</a></li>
+				<li><a href="#">Experiencia</a></li>
+				<li><a href="#">Proyectos</a></li>
+				<li><a href="#">Contacto</a></li>
+			</ul>
+		</nav>
+	</header>
+	<section id="contenido">
+		<div id="b1" class="contenedor"></div>
+		<div id="b2" class="contenedor"></div>
+		<div id="b3" class="contenedor"></div>
+	</section>
+	<footer>Jessé Rangel Portfolio <?php echo date("Y") ?></footer>	
 </body>
 </html>
